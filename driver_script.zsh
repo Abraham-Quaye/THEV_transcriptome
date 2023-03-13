8d73efac911234ed782455c5ea36f9e660faf9f9
Abraham Quaye <quayeabraham29@gmail.com>
2023-03-13 14:17:52 -0600

    Modified files to work with GFF instead of GTF

--- driver_script.zsh
+++ driver_script.zsh
@@ -1,43 +1,47 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 set -e
 
 
-scripts=scripts/zsh
+zsh=scripts/zsh
+r=scripts/r
 #################### ACTIVATE CONDA ENVIRONMENT (RNASEQ) ##################
 eval "$(conda shell.zsh hook)"
 conda activate rnaseq && echo "successfull activation" || exit 1
 
 #################### BUILD THEV GENOMIC INDEX FOR MAPPING WITH HISAT2 ######
-$scripts/build_genome_index.zsh
+$zsh/build_genome_index.zsh
+
+################### REMOVE UNWANTED GFF FEATURES ####################
+R -e "source('$r/makingGFFfile.R')"
 
 
 #################### MAP READS TO THEV GENOME WITH HISAT2 #############
-$scripts/mapping.zsh
+$zsh/mapping.zsh
 
 #################### SORT MAPPED READS AND CONVERT TO .BAM WITH SAMTOOLS ######
-$scripts/sort_sam_to_bam.zsh
+$zsh/sort_sam_to_bam.zsh
 
 #################### INDEX ALL SORTED .BAM FILES ##################
-$scripts/index.zsh
+$zsh/index.zsh
 
 #################### DELETE ALL .SAM FILES ##################
-$scripts/trash_sam.zsh
+$zsh/trash_sam.zsh
 
 #################### CONSTRUCT TRANSCRIPTS WITH STRINGTIE ##############
-$scripts/assemble_transcripts.zsh 
+$zsh/assemble_transcripts.zsh 
 
 #################### BULK MAP READS ##########
-$scripts/bulk_mapping.zsh
+$zsh/bulk_mapping.zsh
 
 #################### BULK SORT .SAM TO .BAM ###########
-$scripts/bulk_sort.zsh
+$zsh/bulk_sort.zsh
 # also deletes all .sam files, leaving only .bam files
 
 #################### BULK COVERAGE #########
-$scripts/bulk_coverage.zsh
+$zsh/bulk_coverage.zsh
 
 #################### BULK DEPTH ############
-$scripts/bulk_depth.zsh
+$zsh/bulk_depth.zsh
 # also removes the .bam files, leaving no bulk mapped files
 
-conda deactivate && echo "rna-seq env deactivated!!" 
+conda deactivate && echo "rna-seq env deactivated!!" '
