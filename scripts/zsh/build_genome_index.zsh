8d0fdb3d3011bfa615773b8e086d89cbf93db9d4
Abraham Quaye <quayeabraham29@gmail.com>
2023-03-13 03:13:00 -0600

    generated gtf files with wrong splice sites

--- scripts/zsh/build_genome_index.zsh
+++ scripts/zsh/build_genome_index.zsh
@@ -8,7 +8,7 @@ idxdir=raw_files/thevgenome_index
 
 ################# CREATE .GFF3 FILE FROM .BED ###########
 echo "Converting .bed file to .gff3 file ..." &&
-agat_convert_bed2gff.pl --bed $filedir/THEVannotated_genesOnly.bed --source abquaye --primary_tag CDS -o $filedir/thev_predicted_genes.gff && 
+agat_convert_bed2gff.pl --bed $filedir/THEVannotated_genesOnly.bed --source abquaye -o $filedir/thev_predicted_genes.gff && 
 echo ".GFF3 file created successfully" || exit 1
 
 ################# CONVERT .GFF3 TO GTF ###########
@@ -19,13 +19,13 @@ echo ".GTF file created successfully" || exit 2
 
 ################# EXTRACT SPLICESITES FROM GTF FILE ################
 echo "extracting splice-sites ..."
-extract_splice_sites.py $filedir/thev_predicted_genes.gtf > $filedir/thev_predicted_genes.ss &&
+extract_splice_sites.py $filedir/thev_predicted_genes.gff > $filedir/thev_predicted_genes.ss &&
 echo "splice-site extraction completed successfully" || exit 3
 
 
 ################# EXTRACT EXONS FROM GTF FILE 
 echo "extracting exons ..."
-extract_exons.py $filedir/thev_predicted_genes.gtf > $filedir/thev_predicted_genes.exons &&
+extract_exons.py $filedir/thev_predicted_genes.gff > $filedir/thev_predicted_genes.exons &&
 echo "exon extration complete" || exit 4
 
 
