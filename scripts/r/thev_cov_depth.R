1392b99dbb8e9188a6b733cfc33d85039cd9be97
Abraham Quaye <quayeabraham29@gmail.com>
2023-03-14 04:10:58 -0600

    Replace .png with .pdf; create correlation graph

--- scripts/r/thev_cov_depth.R
+++ scripts/r/thev_cov_depth.R
@@ -85,7 +85,7 @@ p4 <- depth4hrs %>%
     y = "Mapping Depth"
   ) +
   looks
-ggsave("depth_4hrs.png",
+ggsave("depth_4hrs.pdf",
   plot = p4, path = "results/r/figures",
   width = 10, height = 5
 )
@@ -106,7 +106,7 @@ p12 <- depth12hrs %>%
     y = "Mapping Depth"
   ) +
   looks
-ggsave("depth_12hrs.png",
+ggsave("depth_12hrs.pdf",
   plot = p12, path = "results/r/figures",
   width = 10, height = 5
 )
@@ -127,7 +127,7 @@ p24 <- depth24hrs %>%
     y = "Mapping Depth"
   ) +
   looks
-ggsave("depth_24hrs.png",
+ggsave("depth_24hrs.pdf",
   plot = p24, path = "results/r/figures",
   width = 10, height = 5
 )
@@ -148,7 +148,7 @@ p72 <- depth72hrs %>%
     y = "Mapping Depth"
   ) +
   looks
-ggsave("depth_72hrs.png",
+ggsave("depth_72hrs.pdf",
   plot = p72, path = "results/r/figures",
   width = 10, height = 5
 )
@@ -158,7 +158,7 @@ ggsave("depth_72hrs.png",
 # 1. gridExtra package
 # alltime <- grid.arrange(p4, p12, p24, p72,
 #             ncol = 1, nrow = 4)
-# ggsave("grid_alltimes.png",
+# ggsave("grid_alltimes.pdf",
 #   plot = alltime, path = "results/r/figures",
 #   width = 5, height = 10
 # )
@@ -184,7 +184,7 @@ p_alltime <- (p4 / p12 / p24 / p72) +
     ),
     plot.title.position = "panel"
   )
-ggsave("patch_alltimes.png",
+ggsave("patch_alltimes.pdf",
   plot = p_alltime, path = "results/r/figures",
   width = 10, height = 15
 )
@@ -243,7 +243,7 @@ compare_all <- alltimes %>%
     legend.text = element_text(size = 14, 
                                margin = margin(rep(10, 4)),
                                face = "bold"))
-ggsave("overlay_alltimes.png",
+ggsave("overlay_alltimes.pdf",
   plot = compare_all, path = "results/r/figures",
   width = 15, height = 10
 )
@@ -275,23 +275,18 @@ corr <- cov_all %>%
                   color = "black",
                   linewidth = 0.2) +
       geom_point(size = 15) +
-      geom_textbox(aes(label = glue("**Mean Depth** <br>{round(meandepth,1)}")),
-        nudge_y = 0.25,
-        nudge_x = -0.1,
-        size = 6,
-        hjust = 0.5,
-        show.legend = F, color = "black",
-        box.padding = unit(0, "pt"),
-        width = NULL,
-        box.size = 0) +
-      geom_textbox(aes(label = glue("**Reads** <br>{numreads}")),
-        nudge_y = -0.25,
-        nudge_x = 0.2,
-        size = 6,
-        show.legend = F, color = "black",
-        box.padding = unit(0, "pt"),
-        width = NULL,
-        box.size = 0) +
+      geom_text(aes(label = glue("Mean Depth \n{round(meandepth,1)}")),
+                nudge_y = 0.25,
+                nudge_x = -0.1,
+                size = 6,
+                fontface = "bold",
+                show.legend = F, color = "black") +
+      geom_text(aes(label = glue("Reads \n{numreads}")),
+                fontface = "bold",
+                nudge_y = -0.25,
+                nudge_x = 0.2,
+                size = 6,
+                show.legend = F, color = "black") +
       annotate(geom = "text", x = 2.5e5, y = 5e2, 
                label = glue("R^2 == {round(slmod$adj.r.squared,4)}"),
                parse = T, size = 12) +
@@ -303,15 +298,16 @@ corr <- cov_all %>%
            color = element_blank()) +
       scale_color_manual(values = rainbow(4)) +
       theme(plot.background = element_blank(),
+            plot.margin = margin(rep(20,4)),
             panel.background = element_blank(),
-            panel.grid.major.y = element_line(linewidth = 0.025, 
-                                              color = "black", 
+            panel.grid.major.y = element_line(linewidth = 0.01, 
+                                              color = "grey", 
                                               linetype = "solid"),
             plot.title = element_text(size = 28, face = "bold", 
                                       hjust = 0.5, margin = margin(b = 10)),
         axis.line = element_line(linewidth = 0.4),
         axis.text = element_text(size = 14, color = "black"),
-        axis.title = element_text(size = 18, face = "bold"),
+        axis.title = element_text(size = 22, face = "bold"),
         axis.ticks.length.y = unit(0, "cm"),
         legend.text = element_text(size = 12, face = "bold"),
         legend.key = element_rect(fill = "white"),
@@ -319,6 +315,6 @@ corr <- cov_all %>%
         legend.justification = c(0, 0),
         legend.position = c(0.05, 0.6))
 
-ggsave("correlate_alltimes.png",
+ggsave("correlate_alltimes.pdf",
        plot = corr, path = "results/r/figures",
-       width = 15, height = 10)
+       width = 20, height = 14)
