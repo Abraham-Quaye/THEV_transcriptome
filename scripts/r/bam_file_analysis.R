d25540825518c022611f2a8011da6381695ce422
Abraham Quaye <quayeabraham29@gmail.com>
2023-06-19 15:20:27 -0600

    Update transcript filter script and manuscript

--- scripts/r/bam_file_analysis.R
+++ scripts/r/bam_file_analysis.R
@@ -16,7 +16,7 @@ single_junc_stats <- map_dfr(single_jun_files, read_tsv,
 
 bulk_jun_files <- list.files("results/hisat2/bulk", full.names = T,
                              pattern = "\\w+\\.bed") %>% 
-  setNames(c("t12hrs", "t24hrs", "t4hrs", "t72hrs"))
+  setNames(c("12hpi", "24hpi", "4hpi", "72hpi"))
 
 bulk_junc_stats <- map_dfr(bulk_jun_files, read_tsv,
                            col_names = F,
@@ -28,10 +28,33 @@ bulk_junc_stats <- map_dfr(bulk_jun_files, read_tsv,
 bulk_sum_stats <- bulk_junc_stats %>% 
   group_by(timepoint) %>%
   summarize(total_junctions = n(),
+            reads10  = sum(read_count > 10),
+            reads100  = sum(read_count > 100),
+            reads1000  = sum(read_count > 1000),
             total_reads_supporting = sum(read_count),
-            mean_read_supporting = total_reads_supporting/total_junctions)
+            mean_read_supporting = total_reads_supporting/total_junctions) %>% 
+  mutate(timepoint = factor(timepoint, levels = c("4hpi", "12hpi", "24hpi", "72hpi"))) %>% 
+  arrange(total_junctions)
   
-stats_per_timepoint
+cov_all <- read_tsv("results/hisat2/coverage/bulk_coverage.txt",
+                    comment = "Coverage", show_col_types = FALSE) %>%
+  rename("organism" = "#rname") %>%
+  filter(organism != "#rname") %>%
+  mutate(timepoint = c("4hpi", "12hpi", "24hpi", "72hpi"),
+         timepoint = factor(timepoint,
+                            levels = c("4hpi", "12hpi", "24hpi", "72hpi"))) %>%
+  map_at(c(2:9), as.numeric) %>%
+  as_tibble() %>% 
+  select(Timepoint = timepoint, "Total Mapped" = numreads, "Mean Depth" = meandepth)
   
-  
-  
+total_seq_reads <- read_table("results/hisat2/coverage/bulk_counts.txt",
+                              col_names = T, show_col_types = F)
+
+join_stats <- left_join(cov_all, bulk_sum_stats, by = c("Timepoint" = "timepoint")) %>% 
+  bind_cols(total_seq_reads) %>%
+  mutate(perc_map = (`Total Mapped`/total_reads) * 100) %>% 
+  select(Timepoint, "Total Reads" = total_reads, `Total Mapped`, "Mapped (%)" = perc_map, `Mean Depth`,
+         "Splice Junctions" = total_junctions, "Total Junction Reads" = total_reads_supporting,
+         reads10, reads100, reads1000) %>%
+  map_at(c(2), format, big.mark = ",") %>% as_tibble()
+
--- scripts/r/filter_real_transcripts.R
+++ scripts/r/filter_real_transcripts.R
@@ -17,7 +17,7 @@ merged_gtf <- read_tsv("results/stringtie/all_merged.gtf",
                        col_names = FALSE,
                        show_col_types = FALSE) 
 
-duplicates <- 'gene_id "24S2.11"|gene_id "24S1.11"|gene_id "72S1.9"|gene_id "72S3.10"|gene_id "24S1.2"|transcript_id "24S3.2.5"|gene_id "12S1.2"|transcript_id "24S1.1.1"|gene_id "24S2.1"|transcript_id "24S2.2.5"|transcript_id "72S1.8.1"|transcript_id "72S2.8.1"|transcript_id "4S2.3.1"|transcript_id "72S1.10.1"|transcript_id "24S1.12.1"|transcript_id "72S2.9.1"'
+duplicates <- 'gene_id "24S2.11"|gene_id "24S1.11"|gene_id "72S1.9"|gene_id "72S3.10"|gene_id "24S1.2"|transcript_id "24S3.2.5"|gene_id "12S1.2"|transcript_id "24S1.1.1"|gene_id "24S2.1"|transcript_id "24S2.2.5"|transcript_id "72S1.8.1"|transcript_id "72S2.8.1"|transcript_id "4S2.3.1"|transcript_id "72S1.10.1"|transcript_id "24S1.12.1"|transcript_id "72S2.9.1"|transcript_id "72S1.1.1"|transcript_id "72S3.1.1"|transcript_id "24S3.1.1"|transcript_id "24S1.1.2"|transcript_id "12S1.1.1"|transcript_id "24S2.2.1"|transcript_id "4S1.4.1"'
 
 filter_real_transcripts <- merged_gtf %>% 
   filter(!str_detect(X9,"ref_gene_name")) %>% 
